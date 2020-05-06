7d63a8fee282f28ccaba6a2a1235e1877cf12b8a
zolomohan <mohanzolomon@gmail.com>
2020-05-06 17:28:20 +0530

    Add Title to Post Model

--- posts/api.py
+++ posts/api.py
@@ -2,9 +2,13 @@ from rest_framework import viewsets, permissions
 from .models import Post
 from .serializer import PostSerializer
 
+
 class PostViewSet(viewsets.ModelViewSet):
-  serializer_class = PostSerializer
-  queryset = Post.objects.all()
-  permission_classes = [
-    permissions.AllowAny
-  ]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+	owner = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, null=True)
+	title = models.CharField(max_length=150)
 	due_date = models.DateField()
 	description = models.TextField()
 	active = models.BooleanField(default=True)
