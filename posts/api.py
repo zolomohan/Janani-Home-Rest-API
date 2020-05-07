2a1fd41fc5603a5abb9ca0270bcbe609643b90c2
zolomohan <mohanzolomon@gmail.com>
2020-05-07 20:42:43 +0530

    Restrict Disabling Post to Post Owner

--- posts/api.py
+++ posts/api.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 from .models import Post
 from .serializer import PostSerializer
 
@@ -12,3 +14,13 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def partial_update(self, serializer, pk):
+        post = Post.objects.get(pk=pk)
+        data = self.request.data
+        if(self.request.user == post.owner):
+            if("active" in data):
+                post.active = data['active']
+            post.save()
+            return Response(PostSerializer(post).data, status=202)
+        return Response({"non_field_errors": "Unauthorized to perform this action."}, status=401)
