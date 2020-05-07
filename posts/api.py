01fb650648c1946d0d9a463ee4cab4472d191f7e
zolomohan <mohanzolomon@gmail.com>
2020-05-08 01:01:48 +0530

    Add Toggle Active Post Route

--- posts/api.py
+++ posts/api.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, permissions
-from django.contrib.auth.models import User
 from rest_framework.response import Response
-from .models import Post
+from rest_framework.decorators import action
 from .serializer import PostSerializer
+from django.contrib.auth.models import User
+from .models import Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -15,12 +16,11 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def partial_update(self, serializer, pk):
+    @action(methods=['post'], detail=True)
+    def toggle(self, serializer, pk):
         post = Post.objects.get(pk=pk)
-        data = self.request.data
         if(self.request.user == post.owner):
-            if("active" in data):
-                post.active = data['active']
+            post.active = not post.active
             post.save()
-            return Response(PostSerializer(post).data, status=202)
-        return Response({"non_field_errors": "Unauthorized to perform this action."}, status=401)
+            return Response(status=202)
+        return Response(status=401)
